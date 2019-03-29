a2b788d8948612f08daa3f2b1f26b887c8831c00
boccia <fboccacini@gmail.com>
2019-03-29 16:55:00 +0100

    sync commit

--- InteractionChannel.cpp
+++ InteractionChannel.cpp
@@ -1,12 +1,17 @@
 #include "InteractionChannel.h"
 
+
+void consolePrint(message)
+{
+	Serial.println(message);
+}
 /* Output channels for sensor interaction */
-PrintChannel::PrintChannel(short int pins[], char name[], short int channelType,void (*printFunction)(const char*))
+PrintChannel::PrintChannel(short int pins[], char name[], short int channelType,void (*printFunction)());
 {
 
 	char* _name = name;
 	short int* _pins = pins;
-	void (*print)(const char*) = printFunction;
+	void (*print)() = printFunction;
 
 };
 
@@ -14,13 +19,17 @@ PrintChannel::PrintChannel(short int pins[], char name[], short int channelType,
 /* Control input channer for sensor interaction */
 ControlChannel::ControlChannel(int pins[], char name[], control_input controlInput)
 {
+
+
+
 		char lastReadChar;
 		char* lastReadSentence;
 
 		char* _name = name;
 		short int* _pins = pins;
 		control_input specialButtons = controlInput;
-}
+
+};
 
 // Reads a single charachter
 char ControlChannel::readChar()
@@ -53,45 +62,48 @@ float ControlChannel::readFloat()
 }
 
 /* InteractionChannel is a combo of a PrintChannel and a ControlChannel */
-// InteractionChannel::InteractionChannel(ControlChannel* controlChannel, PrintChannel* printChannel)
-// {
-//
-//     ControlChannel _controlChannel = controlChannel;
-//     PrintChannel _printChannel = printChannel;
-// }
+InteractionChannel::InteractionChannel(ControlChannel controlChannel, PrintChannel printChannel)
+{
+
+    ControlChannel _controlChannel = controlChannel;
+    PrintChannel _printChannel = printChannel;
+};
 
 // Prints a message on its print channel
-// void InteractionChannel::print(char* message)
-// {
-//
-// }
-//
-// // Reads a single charachter from its control channel
-// char InteractionChannel::readChar()
-// {
-//
-// }
-//
-// // Reads a single character from its control channel and transforms it in an integer
-// short int InteractionChannel::readSingleInt()
-// {
-//
-// }
-//
-// // Reads characters from its control channel until it reads the accept char from the configuration
-// char* InteractionChannel::readSentence()
-// {
-//
-// }
-//
-// // Like readSentence, but it convert the output to an integer
-// int InteractionChannel::readInt()
-// {
-//
-// }
-//
-// // Same again, but it converts to float. The dot button marks the decimal point
-// float InteractionChannel::readFloat()
-// {
-//
-// }
+void InteractionChannel::print(char* message)
+{
+
+}
+
+// Reads a single charachter from its control channel
+char InteractionChannel::readChar()
+{
+
+}
+
+// Reads a single character from its control channel and transforms it in an integer
+short int InteractionChannel::readSingleInt()
+{
+
+}
+
+// Reads characters from its control channel until it reads the accept char from the configuration
+char* InteractionChannel::readSentence()
+{
+
+}
+
+// Like readSentence, but it convert the output to an integer
+int InteractionChannel::readInt()
+{
+
+}
+
+// Same again, but it converts to float. The dot button marks the decimal point
+float InteractionChannel::readFloat()
+{
+
+}
+
+
+#endif
--- InteractionChannel.h
+++ InteractionChannel.h
@@ -1,7 +1,6 @@
 #ifndef INTERACTION_CHANNEL_H
 #define INTERACTION_CHANNEL_H
 
-/* Definition of channel types */
 #define CONSOLE 1
 #define KEYPAD 2
 
@@ -11,8 +10,8 @@
 class PrintChannel
 {
 	public:
-		PrintChannel(short int pins[], char name[], short int channelType,void (*printFunction)(const char*));
-		void (*print)(const char*);
+		PrintChannel(short int pins[], char name[], short int channelType,void (*printFunction)());
+		void (*print)();
 
 	private:
 		char* _name;
@@ -30,6 +29,24 @@ typedef struct control_input {
 	char previous;
   char dot;
 };
+
+//////////////////////////////////////////////////////////////////////////////////////////
+/*																																											*/
+/* Definition of control_input for specific controllers.																*/
+/*																																											*/
+/* Values describe, respectively: 																											*/
+/* Accept button, used normally to choose an option or enter a menu											*/
+/* Back button, used to get back to the previous menu																		*/
+/* Exit button, used to get out of all menus to the home display												*/
+/* Next button, used to select the next option																					*/
+/* Previous button, used to select the previous option																	*/
+/* Dot button, used to set decimal point in a float number 															*/
+/*																																											*/
+/* TODO: set correct defaults for all control channels																	*/
+/*																																											*/
+//////////////////////////////////////////////////////////////////////////////////////////
+control_input keypadSpecialChars = {'A','B','C','D','E','F'};
+
 
 /* Control input channer for sensor interaction */
 class ControlChannel
@@ -55,23 +72,22 @@ class ControlChannel
 };
 
 /* InteractionChannel is a combo of a PrintChannel and a ControlChannel */
-// class InteractionChannel
-// {
-//   public:
-//     InteractionChannel(ControlChannel* controlChannel, PrintChannel* printChannel);
-//     void print(char* message);    // Prints a message on its print channel
-//     char readChar();              // Reads a single charachter from its control channel
-//     short int readSingleInt();    // Reads a single character from its control channel and transforms it in an integer
-//
-// 		char* readSentence();         // Reads characters from its control channel until it reads the accept char from the configuration
-//     int readInt();                // Like readSentence, but it convert the output to an integer
-//     float readFloat();            // Same again, but it converts to float. The dot button marks the decimal point
-//
-//
-//   private:
-//     ControlChannel controlChannel;
-//     PrintChannel printChannel;
-// };
+class InteractionChannel
+{
+  public:
+    InteractionChannel(ControlChannel controlChannel, PrintChannel printChannel);
+    void print(char* message);    // Prints a message on its print channel
+    char readChar();              // Reads a single charachter from its control channel
+    short int readSingleInt();    // Reads a single character from its control channel and transforms it in an integer
 
+		char* readSentence();         // Reads characters from its control channel until it reads the accept char from the configuration
+    int readInt();                // Like readSentence, but it convert the output to an integer
+    float readFloat();            // Same again, but it converts to float. The dot button marks the decimal point
+
+
+  private:
+    ControlChannel controlChannel;
+    PrintChannel printChannel;
+};
 
 #endif
